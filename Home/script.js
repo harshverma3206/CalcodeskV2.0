4898e8a112125e1e01b2098f6387829d45652514
Harsh <harshverma3206@gmail.com>
2025-11-16 00:27:21 +0530

    page01Completed

--- Home/script.js
+++ Home/script.js
@@ -1,35 +1,42 @@
 
 // Swiper JavaScript
-const swiperTestimonial = new Swiper('.testimonial__swiper', {
-    loop: true,
-    slidesPerView: 'auto',
-    centeredSlides: 'auto',
-    spaceBetween: 16,
-    grabCursor: true,
-    speed: 600,
-    effect: 'coverflow',
-    coverflowEffect: {
-        rotate:-90,
-        stretch: 0,
-        depth: 600,
-        modifier:.51,
-        slideShadows: false, 
-    },
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
 
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
+function testimonialAnimation() {
 
-    autoplay: {
-        delay:3000,
-        disableOnInteraction: false,
-    },
+    const swiperTestimonial = new Swiper('.testimonial__swiper', {
+        loop: true,
+        slidesPerView: 'auto',
+        centeredSlides: 'auto',
+        spaceBetween: 16,
+        grabCursor: true,
+        speed: 600,
+        effect: 'coverflow',
+        coverflowEffect: {
+            rotate: -90,
+            stretch: 0,
+            depth: 600,
+            modifier: .51,
+            slideShadows: false,
+        },
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
 
-});
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+
+    });
+}
+
+testimonialAnimation();
+
